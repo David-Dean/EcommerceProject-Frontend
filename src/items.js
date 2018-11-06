5894a3a866e3e57161506ee7226b905d2e48b11f
David Dean <daviddean@Davids-MacBook-Pro-2.local>
2018-11-06 16:59:52 -0500

    fifth again

---
+++ src/items.js
@@ -0,0 +1,21 @@
+import React, {Component} from'react';
+import {Route, BrowserRouter, Link} from 'react-router-dom';
+
+class Item extends Component{
+    render(){
+        return (<div className='item'>
+                    <img height="100px" src={this.props.imageLocation}/>
+                    <div>
+                        <div>{this.props.description}</div>
+
+
+                    </div>
+
+
+
+        </div>)
+    }
+}
+
+let ConnectedItems = connect()(Item)
+export default ConnectedItems
