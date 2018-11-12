baee0d5c8ba34364420fda170d79270adfdf369e
David Dean <daviddean@Davids-MacBook-Pro-2.local>
2018-11-11 21:17:35 -0500

    commit

--- src/profile.js
+++ src/profile.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import './App.css';
+import {Link} from 'react-router-dom'
 
 class Profile extends Component{
     constructor(props){
